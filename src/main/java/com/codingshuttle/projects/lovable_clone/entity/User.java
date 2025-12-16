93cab3315796abb11731ccb0cf3ca028e872c226
Rajesh <beherarajesh90@gmail.com>
2025-12-16 22:09:34 +0530

    Initial Commit - lovable clone

---
+++ src/main/java/com/codingshuttle/projects/lovable_clone/entity/User.java
@@ -0,0 +1,19 @@
+package com.codingshuttle.projects.lovable_clone.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User {
+
+    Long id;
+    String email;
+    String passwordHash;
+    String name;
+    String avatarUrl;
+
+}
